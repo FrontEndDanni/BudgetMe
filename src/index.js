04bd2eba1743139f01e3f70a78a4cd216c8c7886
FrontEndDanni <dannikabdull@gmail.com>
2022-11-23 16:11:33 -0800

    put navbar in separate component file and started homepage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Navbar from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BudgetsProvider } from "./contexts/BudgetsContext"
 
@@ -9,6 +10,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BudgetsProvider>
+    <Navbar />
+    <br/>
     <App />
     </BudgetsProvider>
   </React.StrictMode>
